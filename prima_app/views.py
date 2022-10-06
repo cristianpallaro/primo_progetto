b0654a4ed8bd4e24e0fbfc03c9813015a813e5d5
cristianpallaro <114905960+cristianpallaro@users.noreply.github.com>
2022-10-06 11:28:40 +0200

    Initial commit

---
+++ prima_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+def homepage(request):
+    return render(request,"homepage.html")
+
+def welcome(request):
+    return render(request,"welcome.html")
+
+def lista(request):
+    return render(request,"lista.html")
+
+def chi_siamo(request):
+    return render(request,"chi_siamo.html")
+# Create your views here.
