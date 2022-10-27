fb969963635e99f8612f5214af7bd8c65417699b
cristianpallaro <114905960+cristianpallaro@users.noreply.github.com>
2022-10-27 13:54:31 +0200

    aggiornamento

--- prova_pratica_0/urls.py
+++ prova_pratica_0/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from prova_pratica_0.views import index,maxmin,media
 
-app_name="prova_pratica-0"
+app_name="prova_pratica_0"
 urlpatterns=[
     path('',index,name='index'),
     path('maxmin/',maxmin,name='maxmin'),
