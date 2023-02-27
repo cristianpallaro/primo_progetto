eb4972d8d5435f93fb9eee282f95527bb7dd1dd4
cristianpallaro <114905960+cristianpallaro@users.noreply.github.com>
2023-02-27 10:51:38 +0100

    api

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'seconda_app',
     'prova_pratica_0',
     'prova_pratica_1',
+    'news_app',
+   
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -3,14 +3,19 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import index_generale
 
-app_name="seconda_app"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('prima_app/',include('prima_app.urls',namespace="prima_app")),
     path('seconda_app/',include('seconda_app.urls',namespace="seconda_app")),
     path('',index_generale, name="index_generale"),
     path('prova_pratica_0/',include('prova_pratica_0.urls',namespace="prova_pratica_0")),
-     path('prova_pratica_1/',include('prova_pratica_1.urls',namespace="prova_pratica_1")),
+    path('prova_pratica_1/',include('prova_pratica_1.urls',namespace="prova_pratica_1")),
+    path('news_app/',include('news_app.urls',namespace="news_app")),
 
     
+]
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
